177ac82232fcb02af015f126c54b6c89f7386c1d
omid nami <namitutorial@omids-MacBook-Pro.local>
2023-07-28 17:31:58 +0330

    start 3dsky

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+    <main></main>
+    <Footer />
+    </>
+
+  )
+}
